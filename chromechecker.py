e23d289bb100599109f555f2b05cacde6481c994
Zhang Minghao <q83174385@gmail.com>
2018-10-25 14:00:58 +0800

    +增加自动配置运行环境脚本

---
+++ chromechecker.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from xml.dom.minidom import parseString
+from base64 import b64decode
+from urllib import request
+import sys
+import time
+
+info = {
+    "Stable": {
+        "x86": "-multi-chrome",
+        "x64": "x64-stable-multi-chrome",
+        "appid": "{8A69D345-D564-463C-AFF1-A69D9E530F96}"
+    },
+    "Beta": {
+        "x86": "1.1-beta",
+        "x64": "x64-beta-multi-chrome",
+        "appid": "{8A69D345-D564-463C-AFF1-A69D9E530F96}"
+    },
+    "Dev": {
+        "x86": "2.0-dev",
+        "x64": "x64-dev-multi-chrome",
+        "appid": "{8A69D345-D564-463C-AFF1-A69D9E530F96}"
+    },
+    "Canary": {
+        "x86": "",
+        "x64": "x64-canary",
+        "appid": "{4EA16AC7-FD5A-47C3-875B-DBF4A2008C20}"
+    }
+}
+
+update_url = 'https://tools.google.com/service/update2'
+
+def main():
+    print('Chrome开发版本：\n1：Stable\n2：Beta\n3：Dev\n4：Canary')
+    chrome = input('请选择Chrome的开发版本（默认为Stable）：')
+    if chrome == '':
+        chrome = 1
+    else:
+        chrome = int(chrome)
+    if chrome == 1:
+        ver = 'Stable'
+    elif chrome == 2:
+        ver = 'Beta'
+    elif chrome == 3:
+        ver = 'Dev'
+    elif chrome == 4:
+        ver = 'Canary'
+    else:
+        ver = 'Stable'
+    print('已选择开发版本：%s' % ver)
+    
+    print('Chrome编译版本：\n1：x86\n2：x64')
+    arch = input('请选择Chrome的编译版本（默认为x86）：')
+    if arch == '':
+        arch = 1
+    else:
+        arch = int(arch)
+    if arch == 1:
+        arch = 'x86'
+    elif arch == 2:
+        arch = 'x64'
+    print('已选择编译版本：%s' % arch)
+    time.sleep(2)
+
+    #for arch in ['x64','x86']:
+    print('='*20)
+    print(ver, arch)
+    payload = """<?xml version='1.0' encoding='UTF-8'?>
+        <request protocol='3.0' version='1.3.23.9' ismachine='0'
+                 installsource='ondemandcheckforupdate' dedup='cr'
+                 sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}'
+                 requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}'>
+        <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1'/>
+        <os platform='win' version='6.3' arch='"""+arch+"""'/>
+        <app appid='"""+info[ver]['appid']+"""' ap='"""+info[ver][arch]+"""' brand='GGLS'>
+            <updatecheck/>
+        </app>
+        </request>
+    """
+
+    requ = request.Request(url=update_url, data=payload.encode())
+    resp = request.urlopen(requ).read().decode('utf-8')
+    DOMTree = parseString(resp)
+    
+    version = ''
+    for action in DOMTree.getElementsByTagName('action'):
+        if action.getAttribute('run') != '': # kind weird
+            name = action.getAttribute('run')
+        if action.getAttribute('Version') != '':
+            print('version:', action.getAttribute('Version'))
+            version = action.getAttribute('Version')
+
+    download_url = []
+    for url in DOMTree.getElementsByTagName('url'):
+        if url.getAttribute('codebase').startswith('https'):
+            print(url.getAttribute('codebase')+name)
+            download_url.append(url.getAttribute('codebase')+name)
+    chrome_hash = ''
+    for package in DOMTree.getElementsByTagName('package'):
+        print('size: %.2fMB' %(int(package.getAttribute('size'))/1024/1024))
+        print('sha1:', b64decode(package.getAttribute('hash').encode()).hex())
+        print('sha256:', package.getAttribute('hash_sha256'))
+        chrome_hash = b64decode(package.getAttribute('hash').encode()).hex()
+    chrome_info = [download_url, version, chrome_hash]
+    return chrome_info
+
+if __name__=='__main__':
+    if len(sys.argv)==1:
+        main()
---
+++ install.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+'一个招投标网站信息采集工具'
+__author__ = 'Zhang Minghao'
+
+print('安装windows系统下运行环境')
+import urllib, os, zipfile, hashlib
+from html.parser import HTMLParser
+from urllib.request import urlopen, urlretrieve
+
+print('安装第三方库：')
+os.system('pip install configparser selenium')
+
+def cbk(a,b,c):  
+    '''''回调函数 
+    @a:已经下载的数据块 
+    @b:数据块的大小 
+    @c:远程文件的大小 
+    '''  
+    per=100.0*a*b/c  
+    if per>100:  
+        per=100  
+    print('下载进度：%.2f%%' % per, end='\r')
+    
+proxy = input('设置http代理（可选）：')
+if proxy != '':
+    os.environ['http_proxy'] = proxy
+    os.environ['https_proxy'] = proxy
+
+driver = input('是否自动下载Chromedriver（Y/N）：')
+if driver == 'Y' or driver == 'y':
+    print('下载chromedriver')
+    #尝试获取chromedriver最新版本号，获取失败则使用2.43
+    print('获取chromedriver最新版本号：', end='')
+    try:
+        version = urlopen('https://chromedriver.storage.googleapis.com/LATEST_RELEASE').read().decode('utf-8')
+    except urllib.error.URLError:
+        print('获取失败，使用', end='')
+        version = '2.43'
+    print(version)
+
+    #下载chromedriver并解压缩到当前目录
+    url = 'https://chromedriver.storage.googleapis.com/%s/chromedriver_win32.zip' % version
+    try:
+        urlretrieve(url, 'chromedriver_win32.zip', cbk)
+    except:
+        print('下载失败，请手动下载并解压。\n' + url)
+    if os.path.exists('chromedriver_win32.zip'):
+        zip_file = zipfile.ZipFile('chromedriver_win32.zip')
+        print('\nchromedriver下载完成！')
+        for names in zip_file.namelist():
+            zip_file.extract(names)
+        zip_file.close()
+        os.remove('chromedriver_win32.zip')
+
+def CalcSha1(filepath):
+    with open(filepath,'rb') as f:
+        sha1obj = hashlib.sha1()
+        sha1obj.update(f.read())
+        hash = sha1obj.hexdigest()
+    return hash
+
+def download_chrome(chrome_version, download_url):
+    for i in range(len(download_url)):
+        if os.path.exists(chrome_version+'_chrome_installer.exe'):
+            break
+        urlretrieve(download_url[i], chrome_version+'_chrome_installer.exe', cbk)
+        print('\nChrome安装包下载完成，请自行安装！')
+
+
+chrome = input('是否自动下载Chrome浏览器（Y/N）：')
+if chrome == 'Y' or chrome == 'y':
+    import chromechecker
+    chrome_info = chromechecker.main()
+    download_url = chrome_info[0]
+    chrome_version = chrome_info[1]
+    chrome_hash = chrome_info[2]
+    if os.path.exists(chrome_version+'_chrome_installer.exe'):
+        file_hash = CalcSha1(chrome_version+'_chrome_installer.exe')
+        if file_hash != chrome_hash:
+            os.remove(chrome_version+'_chrome_installer.exe')
+            download_chrome(chrome_version, download_url)
+    else:
+        download_chrome(chrome_version, download_url)
