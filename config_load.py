b09ae047772e8d916cfacf455e9782e27b1c64ca
张明浩 <q83174385@gmail.com>
2020-03-14 16:20:45 +0800

    Update config_load.py
    不存在配置文件时自动从模板创建

--- config_load.py
+++ config_load.py
@@ -4,12 +4,17 @@
 '一个招投标网站信息采集工具'
 __author__ = 'Zhang Minghao'
 
-import configparser
+import configparser, os
 
 def load_conf(conf_name='spider.conf'):
     """
     读取配置文件
     """
+    if os.path.exists('.\spider.conf'):
+        pass
+    else:
+        from shutil import copyfile
+        copyfile('.\spider.conf.example','.\spider.conf')
     config = configparser.ConfigParser()
     config.read(conf_name, encoding='utf-8')
     return config
